a30f44c884758ec0fd980c9ebf6a613ff73c6ba6
zzrilakkuma <yc117602@hotmail.com>
2023-04-21 00:28:35 +0800

    add user to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_185001) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_162442) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,7 +28,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_185001) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_185001) do
   end
 
   add_foreign_key "orders", "products"
+  add_foreign_key "orders", "users"
 end
