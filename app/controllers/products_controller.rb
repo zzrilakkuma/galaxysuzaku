283548fe843bed43b464e37d8b096adfcf2491f8
zzrilakkuma <yc117602@hotmail.com>
2023-04-28 01:59:56 +0800

    feat: add some frontend

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,10 @@ class ProductsController < ApplicationController
   end
 
   def add_to_cart
+    product = Product.find(params[:product_id])
     product_id = params[:product_id]
     quantity = params[:quantity].to_i
     add_product_to_cart(product_id, quantity)
-    redirect_to product_path(product_id), notice: 'Product added to cart.'
+    redirect_to product_path(product_id), notice: "#{product.name} added to cart."
   end
 end
